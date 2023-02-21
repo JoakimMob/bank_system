10f3d411bf45b551aa996723b3b306aabb77a92f
aeneidITHS <112550107+aeneidITHS@users.noreply.github.com>
2023-02-21 21:25:48 +0100

    Added test 1 and 2

--- bank-system/src/test/java/com/bank/service/impl/BankServiceImplTest.java
+++ bank-system/src/test/java/com/bank/service/impl/BankServiceImplTest.java
@@ -1,11 +1,30 @@
 package com.bank.service.impl;
 
+import com.bank.BankAccount;
 import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class BankServiceImplTest extends TestCase {
-
-    public void testWithdraw() {
-
+    BankServiceImpl bankService;
+    BankAccount bankAccount;
+    @BeforeAll
+    public void initialize(){
+        bankService = new BankServiceImpl();
+        bankAccount = new BankAccount("Elias", "1");
+    }
+    @Test
+    public void testIfWithdrawWorks() {
+        bankAccount.setBalance(100);
+        bankService.withdraw(bankAccount,100.0);
+        assertEquals(100, bankAccount.getBalance());
+        assertEquals(-100,bankAccount.getPrevTrans());
+    }
+    @Test
+    public void testWithdrawWithBiggerWithdrawAmount(){
+        bankAccount.setBalance(200);
+        bankService.withdraw(bankAccount,210);
+        assertEquals(200,bankAccount.getBalance());
     }
 
     public void testDeposit() {

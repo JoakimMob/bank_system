a7717f3d2c7a6274014ca9650e8fa10e0b3e4187
Hanna Friberg <friberg.hanna@outlook.com>
2023-02-21 23:25:07 +0100

    added initializing method to BankMenuTest

--- bank-system/src/test/java/com/bank/service/impl/BankMenuTest.java
+++ bank-system/src/test/java/com/bank/service/impl/BankMenuTest.java
@@ -1,7 +1,26 @@
 package com.bank.service.impl;
 
+import com.bank.BankAccount;
+import com.bank.BankMenu;
+import com.bank.service.BankService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
 
 public class BankMenuTest {
+    BankMenu bankMenu;
+    BankAccount bankAccount;
+    BankService bankService;
 
+    @BeforeEach
+    void initializeBankAccountAndSetBalance(){
+        bankAccount = new BankAccount("Jane Doe", "1");
+        bankAccount.setBalance(1000.00);
+        bankAccount.setPrevTrans(300.00);
+
+    }
 
 }
